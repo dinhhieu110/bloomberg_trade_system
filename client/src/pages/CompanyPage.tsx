b011c6b88065d73e794c48050477dbb7909bad35
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-31 15:07:18 +0700

    [FS-18][Client] Build company details page

--- client/src/pages/CompanyPage.tsx
+++ client/src/pages/CompanyPage.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { getCompanyDetails } from '../endpoints';
 import { CompanyProfile } from '../interfaces/company';
+import { CompanyDashboard, Sidebar, Tile } from '../components';
 interface ICompanyPageProps {}
 
 const CompanyPage: FC<ICompanyPageProps> = (props) => {
@@ -23,7 +24,20 @@ const CompanyPage: FC<ICompanyPageProps> = (props) => {
   return (
     <>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard ticker={ticker!}>
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Price" subTitle={'$' + company.price.toString()} />
+            <Tile title="DCF" subTitle={'$' + company.dcf.toString()} />
+            <Tile title="Sector" subTitle={company.sector} />
+            {/* <CompFinder ticker={company.symbol} />
+            <TenKFinder ticker={company.symbol} /> */}
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company not found!</div>
       )}
