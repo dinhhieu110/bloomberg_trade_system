19143662174570f8cb09ebb82ed9f8118e6fbcff
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-31 12:37:31 +0700

    [FS-15][Client] Implement api calling - Get company details page

--- client/src/endpoints.tsx
+++ client/src/endpoints.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { CompanySearch } from "./interfaces/company";
+import axios from 'axios';
+import { CompanyProfile, CompanySearch } from './interfaces/company';
 
 interface ISearchRes {
   data: CompanySearch[];
@@ -15,10 +15,28 @@ export const searchCompanies = async (query: string) => {
     return data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      console.error("Error message: ", error.message);
+      console.error('Error message: ', error.message);
     } else {
-      console.error("Unexpected error: ", error);
-      return "An expected error has occurred.";
+      console.error('Unexpected error: ', error);
+      return 'An expected error has occurred.';
+    }
+  }
+};
+
+export const getCompanyDetails = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Error message: ', error.message);
+    } else {
+      console.error('Unexpected error: ', error);
+      return 'An expected error has occurred.';
     }
   }
 };
--- client/src/pages/CompanyPage.tsx
+++ client/src/pages/CompanyPage.tsx
@@ -1,9 +1,34 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { getCompanyDetails } from '../endpoints';
+import { CompanyProfile } from '../interfaces/company';
 interface ICompanyPageProps {}
 
 const CompanyPage: FC<ICompanyPageProps> = (props) => {
-  //   const {} = props || {};
-  return <div>CompanyPage</div>;
+  const { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+  const [serverError, setSeverError] = useState<string>('');
+
+  useEffect(() => {
+    const getCompany = async () => {
+      const result = await getCompanyDetails(ticker!);
+      if (typeof result === 'string') {
+        setSeverError(result);
+      } else if (Array.isArray(result?.data)) {
+        setCompany(result?.data[0]);
+      }
+    };
+    getCompany();
+  }, []);
+  return (
+    <>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found!</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
