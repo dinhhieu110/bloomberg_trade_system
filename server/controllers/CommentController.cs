96130fcde43385c9859305deed35ac07aa582b7b
Willson Tran <willson.tran@avepoint.com>
2025-04-03 16:45:43 +0700

    [FS-37][Server] Setup for comments - Get all comments

---
+++ server/controllers/CommentController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.interfaces;
+using server.mappers;
+
+namespace server.controllers
+  {
+  [Route("api/comments")]
+  [ApiController]
+  public class CommentController : ControllerBase
+  {
+    private readonly ICommentRepository _commentRepo;
+
+    public CommentController(ICommentRepository commentRepo)
+    {
+      _commentRepo = commentRepo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+      var comments = await _commentRepo.GetAllAsync();
+
+      var commentDTOs = comments.Select(i => i.ToCommentDTO());
+
+      return Ok(commentDTOs);
+    }
+  }
+}
---
+++ server/repository/CommentRepository.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using server.data;
+using server.interfaces;
+using server.models;
+
+namespace server.repository
+{
+    public class CommentRepository: ICommentRepository
+    {
+    private readonly ApplicationDbContext _context;
+        public CommentRepository(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<List<Comment>> GetAllAsync()
+    {
+      return await _context.Comments.ToListAsync();
+
+    }
+
+    public Task<Comment> GetByIdAsync(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
